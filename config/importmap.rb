f99d291b5618c832e241257936008a40dde3d377
DiegoCruz02 <a.diegocruz02@gmail.com>
2022-12-07 22:08:36 +0000

    importmap pin three workedbin/importmap pin three

--- config/importmap.rb
+++ config/importmap.rb
@@ -4,3 +4,4 @@ pin "application", preload: true
 pin "@hotwired/stimulus", to: "stimulus.min.js", preload: true
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
 pin_all_from "app/javascript/controllers", under: "controllers"
+pin "three", to: "https://ga.jspm.io/npm:three@0.147.0/build/three.module.js"
